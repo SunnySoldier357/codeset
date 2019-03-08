18b73eb174600642e562494079f32ed68800584e
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-03-08 07:09:36 -0800

    Move Option to Models/ & remove unnecessary items

--- Models/Options.cs
+++ Models/Options.cs
@@ -1,27 +1,15 @@
 using CommandLine;
-using CommandLine.Text;
 
-namespace codeset
+namespace codeset.Models
 {
     public class Options
     {
         //* Public Properties
-
-        [ParserState]
-        public IParserState LastParserState { get; set; }
 
         [Option("extfile",
             HelpText = "The txt file that contains a list of extensions.")]
         public string ExtensionFile { get; set; }
 
         public string SettingFile { get; set; }
-
-        //* Public Methods
-
-        [HelpOption]
-        public string GetUsage()
-        {
-            
-        }
     }
 }
