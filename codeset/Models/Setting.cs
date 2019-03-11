584c69d0f75a0156e048ec820c224f9cba98244f
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-03-11 15:39:06 -0700

    Checking for OS in Settings.cs

--- codeset/Models/Setting.cs
+++ codeset/Models/Setting.cs
@@ -1,12 +1,10 @@
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace codeset.Models
 {
     public class Setting
     {
-        //* Private Properties
-        private string value;
-
         //* Public Properties
         public string Key { get; }
 
@@ -15,18 +13,39 @@ namespace codeset.Models
         public string Instruction { get; set; }
 
         //* Constructors
-        public Setting()
+        public Setting(List<string> lines)
         {
-            Value = getValueForOs();
+            if (lines.Count == 1)
+                Value = getValueForOs(lines[0]);
+            else
+            {
+                // Convert lines to a single string with /n separating lines
+            }
         }
 
         //* Private Methods
-        private string getValueForOs()
+        private OSPlatform getOS()
         {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return OSPlatform.Windows;
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                return OSPlatform.OSX;
+
+            return OSPlatform.Linux;
+        }
+
+        private string getValueForOs(string value)
+        {
+            string result = "";
+
             // If value is formatted like Windows:custom; Linux:native then extract
             // the correct value for the current OS
+            if (Value.Contains(';'))
+            {
+                string[] items = value.Split(';');
+            }
 
-            return "";
+            return result;
         }
     }
 }
