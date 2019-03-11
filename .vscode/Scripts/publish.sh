071255a55c42b15024866cb8b93028f67ad6c089
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-03-11 08:25:54 -0700

    Move application publish code to tasks.json

---
+++ .vscode/Scripts/publish.sh
@@ -0,0 +1,3 @@
+cd codeset/
+dotnet publish -c Release -r linux-x64
+chmod 777 bin/Release/netcoreapp2.1/linux-x64/publish/codeset
--- codeset/Program.cs
+++ codeset/Program.cs
@@ -8,11 +8,6 @@ namespace codeset
     {
         static void Main(string[] args)
         {
-            //* Steps to test the application
-            // dotnet publish -c release -r linux-x64
-            // chmod 777 bin/release/netcoreapp2.1/linux-x64/publish/codeset
-            // bin/release/netcoreapp2.1/linux-x64/publish/codeset
-
             var result = FileWrapper.ReadExtensions("/home/sandeepsingh/Repos/Configuration/VS Code/extensions.txt");
 
             var parsed = Parser.Default
