95dc1199036f41bde3d40b434320e14cf1892f2b
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-04-05 18:05:51 -0700

    Update CodeWrapperTest

--- codesetTest/CodeWrapperTest.cs
+++ codesetTest/CodeWrapperTest.cs
@@ -1,21 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
+
 using codeset.Models;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace codesetTest
 {
     [TestClass]
     public class CodeWrapperTest
     {
+        /// <summary>
+        /// <para>
+        /// Tests if the InstallExtension() method can correctly handle null
+        /// extension name.
+        /// </para>
+        /// <para>
+        /// Input: null for extension
+        /// </para>
+        /// <para>
+        /// Expected Output: ArgumentNullException thrown
+        /// </para>
+        /// </summary>
         [TestMethod]
         public void InstallExtensionNullTest()
         {
             bool caughtException = false;
-            CodeWrapper code = new CodeWrapper();
-
+            
             try
             {
+                CodeWrapper code = new CodeWrapper();
                 code.InstallExtension(null);
             }
             catch (ArgumentNullException)
@@ -30,17 +43,29 @@ namespace codesetTest
             Assert.IsTrue(caughtException);
         }
 
+        /// <summary>
+        /// <para>
+        /// Tests if the InstallExtension() method can correctly handle empty
+        /// extension name.
+        /// </para>
+        /// <para>
+        /// Input: "" for extension
+        /// </para>
+        /// <para>
+        /// Expected Output: ArgumentException thrown
+        /// </para>
+        /// </summary>
         [TestMethod]
         public void InstallExtensionEmptyTest()
         {
             bool caughtException = false;
-            CodeWrapper code = new CodeWrapper();
 
             try
             {
+                CodeWrapper code = new CodeWrapper();
                 code.InstallExtension("");
             }
-            catch (IOException)
+            catch (ArgumentException)
             {
                 caughtException = true;
             }
@@ -52,14 +77,26 @@ namespace codesetTest
             Assert.IsTrue(caughtException);
         }
 
+        /// <summary>
+        /// <para>
+        /// Tests if the InstallAllExtension() method can correctly handle null
+        /// path.
+        /// </para>
+        /// <para>
+        /// Input: null for path
+        /// </para>
+        /// <para>
+        /// Expected Output: ArgumentNullException thrown
+        /// </para>
+        /// </summary>
         [TestMethod]
         public void InstallAllExtensionsNullTest()
         {
             bool caughtException = false;
-            CodeWrapper code = new CodeWrapper();
-
+            
             try
             {
+                CodeWrapper code = new CodeWrapper();
                 code.InstallAllExtensions(null);
             }
             catch (ArgumentNullException)
@@ -74,22 +111,37 @@ namespace codesetTest
             Assert.IsFalse(caughtException);
         }
 
+        /// <summary>
+        /// <para>
+        /// Tests if the InstallExtension() method can correctly handle empty path.
+        /// </para>
+        /// <para>
+        /// Input: "" for path
+        /// </para>
+        /// <para>
+        /// Expected Output: IOException thrown
+        /// </para>
+        /// </summary>
         [TestMethod]
         public void InstallAllExtensionsEmptyTest()
         {
-            bool result = false;
-            CodeWrapper code = new CodeWrapper();
+            bool caughtException = false;
 
             try
             {
-                result = code.InstallAllExtensions("");
+                CodeWrapper code = new CodeWrapper();
+                code.InstallAllExtensions("");
+            }
+            catch (IOException)
+            {
+                caughtException = true;
             }
             catch (Exception e)
             {
                 Assert.Fail(e.Message);
             }
 
-            Assert.IsFalse(result);
+            Assert.IsTrue(caughtException);
         }
     }
 }
