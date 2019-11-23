42b4897bf3ce670547cedebbc60afae96eef0614
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-11-23 16:49:56 +0800

    [codesetTest] Template to test updating settings

--- codesetTest/Tests/Services Test/Wrappers Test/VsCodeWrapperTest.cs
+++ codesetTest/Tests/Services Test/Wrappers Test/VsCodeWrapperTest.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using Newtonsoft.Json.Linq;
+
+using codeset.Models;
 using codeset.Services.Wrappers;
+using codeset.Services;
+
+using codesetTest.Utilities;
 
 namespace codesetTest.Tests.ServicesTest.WrappersTest
 {
@@ -71,9 +79,115 @@ namespace codesetTest.Tests.ServicesTest.WrappersTest
             Assert.IsFalse(extensionsAfter.Contains(extension));
         }
 
-        // TODO: Add tests for UpdateSettings()
+        [TestMethod]
+        public void GetUserSettingsTest()
+        {
+            // Arrange
+            string fileName = "settings";
+
+            var platformService = new MockPlatformService(OSPlatform.Linux);
+
+            JObject settings = createSetting(platformService);
+
+            string path = FileUtility.CreateFile(fileName, FileExtension.Json,
+                settings.ToString().Split('\n'));
+
+            var configWrapper = new MockConfigWrapper();
+            var terminalWrapper = new MockTerminalWrapper();
+            var settingsService = new MockSettingsService(null, path);
+
+            var codeWrapper = new VsCodeWrapper(configWrapper, terminalWrapper,
+                settingsService);
+
+            // Act
+            var result = codeWrapper.GetUserSettings();
+
+            // Assert
+            Assert.AreEqual(settings.ToString(), result.ToString(), false);
+
+            // Cleanup
+            FileUtility.DeleteFile(fileName, FileExtension.Json);
+        }
+
+        [TestMethod]
+        public void WriteUserSettingsTest()
+        {
+            // Arrange
+            string fileName = "settings";
+
+            var platformService = new MockPlatformService(OSPlatform.Linux);
+
+            JObject settings = createSetting(platformService);
+
+            string[] expected = new string[]
+            {
+                "{",
+                "\t//* Required",
+                "\t\"testKey 1\": \"item 1\"",
+                "\t\"testKey 2\": \"item 2\"",
+                "}",
+            };
+            string expectedString = string.Join('\n', expected);
+
+            string path = FileUtility.CreateFile(fileName, FileExtension.Json,
+                new string[] { "" });
+
+            var configWrapper = new MockConfigWrapper();
+            var terminalWrapper = new MockTerminalWrapper();
+            var settingsService = new MockSettingsService(null, path);
+
+            var codeWrapper = new VsCodeWrapper(configWrapper, terminalWrapper,
+                settingsService);
+
+            // Act
+            codeWrapper.WriteUserSettings(settings);
+            string result = FileUtility.ReadFile(fileName, FileExtension.Json);
+
+            // Assert
+            Assert.AreEqual(expectedString, result, false);
+        }
+
+        [TestMethod]
+        public void UpdateSettingsTest()
+        {
+            // Arrange
+            string fileName = "settings";
+
+            var platformService = new MockPlatformService(OSPlatform.Linux);
+
+            JObject settings = createSetting(platformService);
+
+            string path = FileUtility.CreateFile(fileName, FileExtension.Json,
+                settings.ToString().Split('\n'));
+
+            var configWrapper = new MockConfigWrapper();
+            var terminalWrapper = new MockTerminalWrapper();
+            var settingsService = new MockSettingsService(null, path);
+
+            var codeWrapper = new VsCodeWrapper(configWrapper, terminalWrapper,
+                settingsService);
+
+            // TODO: Proper Testing
+
+            // Act
+            codeWrapper.UpdateSettings();
+        }
 
         //* Private Methods
+        private JObject createSetting(IPlatformService platformService)
+        {
+            return JObject.FromObject(new Dictionary<string, List<Setting>>
+            {
+                ["Required"] =
+                {
+                    new Setting("testKey.one", JToken.FromObject("item 1"),
+                        null, platformService),
+                    new Setting("testKey.two", JToken.FromObject("item 2"),
+                        null, platformService)
+                }
+            });
+        }
+
         private void setUpInstallExtension(string command)
         {
             // Arrange
